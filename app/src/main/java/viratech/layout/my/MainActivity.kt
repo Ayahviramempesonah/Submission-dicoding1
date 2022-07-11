55954e0252a17f72c7c0bcd157c7b5e2206ca56e
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2022-07-12 00:57:04 +0700

    no show datanya bos,, solve yah

--- app/src/main/java/viratech/layout/my/MainActivity.kt
+++ app/src/main/java/viratech/layout/my/MainActivity.kt
@@ -1,13 +1,59 @@
 package viratech.layout.my
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvUser: RecyclerView
+    private val list = ArrayList<User>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+
+
+
+        rvUser = findViewById(R.id.row_user)
+        rvUser.setHasFixedSize(true)
+
+        list.addAll(listUseres)
+        showRecycler()
+
+
     }
+
+
+    private val listUseres: ArrayList<User>
+        get() {
+
+            val dataname = resources.getStringArray(R.array.name)
+            val dataUserName = resources.getStringArray(R.array.username)
+            val dartaPhoto = resources.obtainTypedArray(R.array.avatar)
+
+            val listUserNya = ArrayList<User>()
+            for (i in dataname.indices) {
+                val user = User(dataname[i], dataUserName[i], dartaPhoto.getResourceId(i, -1))
+                listUserNya.add(user)
+            }
+            return listUserNya
+
+        }
+
+    private fun showRecycler() {
+
+        rvUser.layoutManager = LinearLayoutManager(this)
+        val listUserAdapter = UserAdapter(list)
+        rvUser.adapter = listUserAdapter
+
+
+    }
+
+
 }
--- app/src/main/java/viratech/layout/my/UserAdapter.kt
+++ app/src/main/java/viratech/layout/my/UserAdapter.kt
@@ -1,4 +1,60 @@
 package viratech.layout.my
 
-class UserAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapter (private val listUser: ArrayList<User>) : RecyclerView.Adapter<UserAdapter.ListViewholder>(){
+    class ListViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgPhoto: ImageView = itemView.findViewById(R.id.imageView)
+        var tvName : TextView = itemView.findViewById(R.id.textView_name)
+        var tvDetail: TextView = itemView.findViewById(R.id.textView_detail)
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewholder {
+
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_user,parent,false)
+        return ListViewholder(view)
+
+
+
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+
+
+
+
+
+    override fun onBindViewHolder(holder: ListViewholder, position: Int) {
+
+
+        val (name,description,photo) = listUser[position]
+
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvDetail.text=description
+        holder.tvName.text=name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
