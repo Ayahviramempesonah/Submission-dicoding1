dcaa9a841d3cebf2e89ee78b0f3bc953d02608ac
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2022-08-07 13:10:50 +0700

    belum penerapan parcelable

--- app/src/main/java/viratech/layout/my/HalmanDetail.kt
+++ app/src/main/java/viratech/layout/my/HalmanDetail.kt
@@ -15,14 +15,14 @@ class HalmanDetail : AppCompatActivity() {
 
       val tvUserName=  findViewById<TextView>(R.id.textView)
         val tvPhoto=  findViewById<ImageView>(R.id.circleImageView)
-        val tvNameDetail=  this.findViewById<TextView>(R.id.textView2)
+       val tvNameDetail=  findViewById<TextView>(R.id.textView2)
 
 
        val name =intent.getStringExtra("Name")
         val icon = intent.getIntExtra("PHOTO",0)
-        val desc= intent.getStringExtra("Desc")
+       val desc= intent.getStringExtra("Desc")
      tvPhoto.setImageResource(icon)
-       tvUserName.text = name
-       tvNameDetail.text=desc
+       tvUserName.text =  "NAMA= "+name
+       tvNameDetail.text= "USERNAME= " + "@"+desc
     }
 }
